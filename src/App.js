d5afdaebaf51e0b0f8d4cfbd07b2d1ea452d92ef
mal <1881633+akmalkhadir@users.noreply.github.com>
2019-02-25 19:13:25 +0000

    title bar renders passed children text

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { CssBaseline } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+import TitleBar from './components/TitleBar'
 
+const drawerWidth = 240
+
+const styles = theme => ({
+  root: {
+    display: 'flex'
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3
+  }
+})
 class App extends Component {
+  state = {
+    mobileOpen: false
+  }
+
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }))
+  }
+
   render () {
+    const { classes, theme } = this.props
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classes.root}>
+        <CssBaseline />
+        <TitleBar handleDrawerToggle={this.handleDrawerToggle} header={`Humanise Chat`} />
       </div>
     )
   }
 }
 
-export default App
+export default withStyles(styles, { withTheme: true })(App)
